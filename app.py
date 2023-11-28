46b8b3edad94074b4ca0e024ce12d196bce8d346
AryaAdithyan <120321472+AryaAdithyan@users.noreply.github.com>
2023-11-28 15:31:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,8 +37,8 @@ def main():
     product_name = st.sidebar.selectbox("Select Product", ["M01AB", "M01AE", "N02BA", "N02BE", "N05B", "N05C", "R03", "R06"])
     num_intervals = st.sidebar.slider("Select Number of Intervals for Forecasting", min_value=1, max_value=30, value=7)
 
-    start_date = st.sidebar.date_input("Select Start Date", datetime.today() - timedelta(days=365))
-    end_date = st.sidebar.date_input("Select End Date", datetime.today())
+    start_date = pd.Timestamp(st.sidebar.date_input("Select Start Date", datetime.today() - timedelta(days=365)))
+    end_date = pd.Timestamp(st.sidebar.date_input("Select End Date", datetime.today()))
 
     if start_date >= end_date:
         st.error("End date must be after start date.")
