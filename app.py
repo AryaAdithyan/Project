08d36c8e84e10be9f95d627be0fc6c41c40607bd
AryaAdithyan <120321472+AryaAdithyan@users.noreply.github.com>
2023-11-29 09:01:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def train_autoarima_model(data):
     return model_autoarima
 
 # Function to generate forecast
-def generate_forecast(df, product_name, forecasting_frequency, num_intervals, start_date, end_date):
+def generate_forecast(df, product_name, forecasting_frequency, num_intervals):
     # Train ARIMA model for short-term forecasting
     model_arima = train_arima_model(df[product_name])
 
@@ -147,17 +147,10 @@ def main():
     product_name = st.sidebar.selectbox("Select Product", ["M01AB", "M01AE", "N02BA", "N02BE", "N05B", "N05C", "R03", "R06"])
     num_intervals = st.sidebar.slider("Select Number of Intervals for Forecasting", min_value=1, max_value=30, value=7)
 
-    start_date = pd.Timestamp(st.sidebar.date_input("Select Start Date", datetime.today() - timedelta(days=365)))
-    end_date = pd.Timestamp(st.sidebar.date_input("Select End Date", datetime.today()))
-
-    if start_date >= end_date:
-        st.error("End date must be after start date.")
-        return
-
     # Style the button
     generate_button = st.sidebar.button("Generate Forecast", key="generate_button")
     if generate_button:
-        generate_forecast(df, product_name, forecasting_frequency, num_intervals, start_date, end_date)
+        generate_forecast(df, product_name, forecasting_frequency, num_intervals)
         st.sidebar.success("Forecast generated successfully!")
 
     # Determine the dataset based on the selected frequency
